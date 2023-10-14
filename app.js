3bdbac60d2880084c2d519fb6c6c727ced13a79a
SaweraTahir3 <141661220+SaweraTahir3@users.noreply.github.com>
2023-10-14 23:19:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+// chapter 1 //
+// Ex 1.1 //
+
+console.log("sawera here")
+
+// Ex :1.3 //
+
+console.log("hello world!!!");
+console.log("hiiiiiii")
+// console.log("fatima shahid");
+
+// Ex:1.4//
+console.log("let d = 10")
+// console.log("10"); result not found
+let d = 10
+prompt("how are you !!!!????") 
+console.log("random.math")
+       
+        // chapter:2 //
+        // EX:2.1 //
+     
+      //   let str1 = 'laurence';
+      //   let str2 = "svekis";
+      //   let val1 = undefined;
+      //   let val2 = null;
+      //   let myNum = 1000;
+        
+        console.log(typeof str1); // Output: "string"
+        console.log(typeof str2); // Output: "string"
+        console.log(typeof val1); // Output: "undefined"
+        console.log(typeof val2); // Output: "object" (Note: typeof null returns "object")
+        console.log(typeof myNum); // Output: "number"
+        
+      //   Ex2.2 //
+
+      const yourName = "sawera";
+      const yourAge = 21;  
+   const canCodeJavaScript = true;  
+   console.log('My name is ${sawera}, I am ${21} years old, and I can ${ yescanCodeJavaScript ? code JavaScript}.')
+       
+   // ex2.3//
+let a = window.prompt("Value 1?");
+let b = window.prompt("Value 2?");
+a = Number(a);
+b = Number(b);
+let hypotenuseVal = ((a * a) + (b * b))**0.5;
+console.log(hypotenuseVal);
+
+// ex2.4//
+let e= 4;
+let o= 11;
+let c = 21;
+ e = e+ o;
+e = o/ c;
+c = c % o;
+console.log(e, o, c);
+
+    // project 1//
+//Convert miles to kilometers. 
+//1 mile equals 1.60934 kilometers.
+let myDistanceMiles = 130;
+let myDistanceKM = myDistanceMiles * 1.60934;
+console.log("The distance of " + myDistanceMiles + " miles is equal to " + myDistanceKM + " kilometers");
+
+      // project2//
+//1 inch = 2.54 centimetres.
+//2.2046 pounds in a kilo
+let inches = 72;
+let pounds = 180;
+let weight = pounds / 2.2046; // in kilos
+let height = inches * 2.54; // height in centimetres
+console.log(weight, height);
+let bmi = weight/(height/100*height/100);
+console.log(bmi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
